2c516e12160794935674c7e4b01a9449cfad6cb1
Alex Khaustovych <khaustovych.alex@gmail.com>
2021-01-05 14:37:39 +0200

    removed private data

--- bot.py
+++ bot.py
@@ -11,7 +11,6 @@ db = cluster['JunBot_database']
 collection = db['user_data']
 
 bot = telebot.TeleBot(API_TOKEN)
-
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 url = 'https://jobs.dou.ua/first-job/'
 
